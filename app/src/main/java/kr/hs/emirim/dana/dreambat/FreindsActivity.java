c9844c1b73a4897661462f40a214f085e67cf4f8
KangEunseo <jbogry7@gmail.com>
2019-10-18 00:34:22 +0900

    Second commit

--- app/src/main/java/kr/hs/emirim/dana/dreambat/FreindsActivity.java
+++ app/src/main/java/kr/hs/emirim/dana/dreambat/FreindsActivity.java
@@ -1,15 +1,47 @@
 package kr.hs.emirim.dana.dreambat;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+import kr.hs.emirim.dana.dreambat.Adapter.ListViewAdapter;
 
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
 
 public class FreindsActivity extends FragmentActivity {
+
+    private ListView listView;
+    private ListViewAdapter adapter;
+
+    private int[] img = {R.drawable.profile,R.drawable.profile,R.drawable.profile};
+    private String[] name = {"뽀글깨비","뾰족깨비","쭉쭉깨비"};
+    private String[] id = {"응원챗봇","미션챗봇","음악챗봇"};
+    private String[] content = {"너의 꿈을 응원해~", "꿈을 이루기 위한 미션과 테스트를 수행해봐!", "꿈에 대한 음악을 들려줄게♪"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_freinds);
+
+        listView = (ListView)findViewById(R.id.listView);
+
+        adapter = new ListViewAdapter();    //변수 초기화
+        listView.setAdapter(adapter);   //어댑터 할당
+
+        //adapter를 통한 값 전달
+        for(int i=0; i<img.length;i++){
+            adapter.addVO(ContextCompat.getDrawable(this,img[i]),name[i], id[i], content[i]);
+        }
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/kr/hs/emirim/dana/dreambat/ListVO/ListVO.java
@@ -0,0 +1,42 @@
+package kr.hs.emirim.dana.dreambat.ListVO;
+
+import android.graphics.drawable.Drawable;
+
+public class ListVO {
+    private Drawable img;
+    private String name;
+    private String id;
+    private String content;
+
+    public Drawable getImg() {
+        return img;
+    }
+
+    public void setImg(Drawable img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
